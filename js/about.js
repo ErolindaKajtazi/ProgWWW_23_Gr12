adee46babc7533610565e06d079193d73a8f385a
Erolinda Kajtazi <erolindakajtazi@gmail.com>
2024-01-11 23:01:29 +0100

    making some changes

---
+++ js/about.js
@@ -0,0 +1,48 @@
+function readMore2() {
+  var partToHide = document.getElementById("container2");
+  var changePhoto = document.getElementById("image2");
+  if (partToHide.style.display === "block") {
+    partToHide.style.display = "none";
+    changePhoto.src = "../assets/down-arrow.png";
+  } else {
+    partToHide.style.display = "block";
+    changePhoto.src = "../assets/up-arrow.png";
+  }
+}
+
+function readMore3() {
+  var partToHide = document.getElementById("container3");
+  var changePhoto = document.getElementById("image3");
+  if (partToHide.style.display === "block") {
+    partToHide.style.display = "none";
+    changePhoto.src = "../assets/down-arrow.png";
+  } else {
+    partToHide.style.display = "block";
+    changePhoto.src = "../assets/up-arrow.png";
+  }
+}
+
+function readMore4() {
+  var partToHide = document.getElementById("container4");
+  var changePhoto = document.getElementById("image4");
+  if (partToHide.style.display === "block") {
+    partToHide.style.display = "none";
+    changePhoto.src = "../assets/down-arrow.png";
+  } else {
+    partToHide.style.display = "block";
+    changePhoto.src = "../assets/up-arrow.png";
+  }
+}
+
+function readMore5() {
+  var partToHide = document.getElementById("container5");
+  var changePhoto = document.getElementById("image5");
+
+  if (partToHide.style.display === "block") {
+    partToHide.style.display = "none";
+    changePhoto.src = "../assets/down-arrow.png";
+  } else {
+    partToHide.style.display = "block";
+    changePhoto.src = "../assets/up-arrow.png";
+  }
+}
